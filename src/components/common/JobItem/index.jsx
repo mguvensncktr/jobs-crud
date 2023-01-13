6f8ad149a12947d9ea5657390fdfbef7123e3926
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-13 19:15:53 +0300

    feat: create JobItem component

---
+++ src/components/common/JobItem/index.jsx
@@ -0,0 +1,36 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { getJobDate } from "../../../utils/getJobDate";
+import styles from "./styles";
+
+const JobItem = ({ job }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "#DB4914", fontSize: 16, fontWeight: "bold" }}>
+        {job.job_title}
+      </Text>
+      <Text
+        style={{
+          color: "#a5adbe",
+          fontSize: 14,
+          fontWeight: "600",
+          marginVertical: 5,
+        }}
+      >
+        {job.job_owner}
+      </Text>
+      <Text
+        style={{ color: "white", marginVertical: 5 }}
+        numberOfLines={2}
+        ellipsizeMode="tail"
+      >
+        {job.job_description}
+      </Text>
+      <Text style={{ color: "#9f9fa1", textAlign: "right", marginTop: 5 }}>
+        {getJobDate(job.job_start_date, job.job_end_date)}
+      </Text>
+    </View>
+  );
+};
+
+export default JobItem;
---
+++ src/components/common/JobItem/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        minHeight: 100,
+        backgroundColor: '#1D212D',
+        borderRadius: 20,
+        marginBottom: 10,
+        padding: 15,
+    }
+});
+
+export default styles;
