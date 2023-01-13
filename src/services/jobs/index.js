190bc835dd33fd052f67f8ba1f1d0c2037194fee
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 00:03:59 +0300

    feat: add a new axios req. for adding a new job

--- src/services/jobs/index.js
+++ src/services/jobs/index.js
@@ -4,3 +4,14 @@ export const getAllJobs = async () => {
     const { data: { jobs } } = await HTTPClient.get("/get_jobs.php");
     return jobs;
 };
+
+export const addNewJob = async (title, owner, startDate, endDate, description) => {
+    const { data } = await HTTPClient.post("/insert_job.php", {
+        job_title: title,
+        job_owner: owner,
+        job_start_date: startDate,
+        job_end_date: endDate,
+        job_description: description
+    });
+    return data;
+}
