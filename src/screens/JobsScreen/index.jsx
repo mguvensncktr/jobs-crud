dd8441f27ccab719a090122c9cecf989ece8136a
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 00:03:39 +0300

    feat: add refreshJobs to JobsScreen to use refreshcontrol with flatlist

--- src/screens/JobsScreen/index.jsx
+++ src/screens/JobsScreen/index.jsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, RefreshControl, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getAllJobs } from "../../services/jobs";
 import Loading from "../../components/common/Loading";
@@ -8,26 +8,50 @@ import AddJobFAB from "../../components/common/AddJobFAB";
 const JobsScreen = () => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [refreshLoading, setRefreshLoading] = useState(false);
 
-  useEffect(() => {
+  const fetchJobs = () => {
     setLoading(true);
     getAllJobs()
       .then(setJobs)
       .finally(() => setLoading(false));
+  };
+
+  const refreshJobs = () => {
+    setRefreshLoading(true);
+    getAllJobs()
+      .then(setJobs)
+      .finally(() => setRefreshLoading(false));
+  };
+
+  useEffect(() => {
+    fetchJobs();
   }, []);
 
+  const listEmptyComponent = () => {
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>
+          İlanda olan hiç iş bulamadık!
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <>
+    <View style={{ flex: 1, backgroundColor: "#1D212D", paddingBottom: 10 }}>
       {loading ? (
         <Loading />
       ) : (
-        <View style={{ flex: 1, backgroundColor: "white", paddingBottom: 10 }}>
+        <>
           <View
             style={{
               width: "100%",
               height: 100,
               backgroundColor: "#1D212D",
               justifyContent: "center",
+              borderBottomWidth: 1,
+              borderBottomColor: "#292D32",
             }}
           >
             <Text
@@ -36,9 +60,10 @@ const JobsScreen = () => {
                 marginLeft: 20,
                 fontSize: 24,
                 fontWeight: "bold",
+                marginTop: 10,
               }}
             >
-              Find a Job
+              İş Ara
             </Text>
           </View>
           <FlatList
@@ -48,11 +73,19 @@ const JobsScreen = () => {
             showsVerticalScrollIndicator={false}
             renderItem={({ item }) => <JobItem job={item} />}
             style={{ flex: 1, paddingHorizontal: 15, paddingTop: 10 }}
+            ListEmptyComponent={listEmptyComponent}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshLoading}
+                tintColor={"#DB4914"}
+                onRefresh={refreshJobs}
+              />
+            }
           />
           <AddJobFAB />
-        </View>
+        </>
       )}
-    </>
+    </View>
   );
 };
 
