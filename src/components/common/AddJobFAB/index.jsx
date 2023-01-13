ff739bbd4a1a49740655fb5f38f7dcfca61fd035
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 00:00:47 +0300

    feat: add navigation to FAB

--- src/components/common/AddJobFAB/index.jsx
+++ src/components/common/AddJobFAB/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import styles from "./styles";
 import { FontAwesome } from "@expo/vector-icons";
 
 const AddJobFAB = () => {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate("AddJob");
+  };
+
   return (
-    <TouchableOpacity style={styles.addJobFAB} activeOpacity={0.4}>
+    <TouchableOpacity
+      style={styles.addJobFAB}
+      activeOpacity={0.4}
+      onPress={onPress}
+    >
       <FontAwesome name="plus" size={24} color="#1D212D" />
     </TouchableOpacity>
   );
