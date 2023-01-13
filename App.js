a02a8afbebc016e1ca4e157d94071801a6f7b22b
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-13 19:14:56 +0300

    refactor: display JobsScreen on App.js

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import JobsScreen from './src/screens/JobsScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <JobsScreen />
       <StatusBar style="auto" />
-    </View>
+    </>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
