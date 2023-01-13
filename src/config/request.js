52faad9b5316e70e842cd14ca78ad3ed623759d1
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-13 19:14:13 +0300

    refactor: change baseURL

--- src/config/request.js
+++ src/config/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const HTTPClient = axios.create({
-    baseURL: 'https://mobiloby.click/test/',
+    baseURL: 'https://mobiloby.click/test',
 });
 
 HTTPClient.defaults.headers.common["Content-Type"] = "application/json";
