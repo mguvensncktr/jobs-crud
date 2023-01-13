efe753444437f046c349399eab7492aec0bf1213
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 02:14:09 +0300

    test: add unit test for Loading component

---
+++ src/components/common/Loading/Loading.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import Loading from '../Loading';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+describe('Loading', () => {
+    it('should render correctly', () => {
+        const { getByTestId } = render(
+            <NavigationContainer>
+                <Loading />
+            </NavigationContainer>
+        );
+        expect(getByTestId('loading')).toBeTruthy();
+    });
+});
