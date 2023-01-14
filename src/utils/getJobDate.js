f9158a15161ffeddfd93da9f6feeadd6b10a6f03
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 11:22:34 +0300

    refactor: refactor some logic on getjobdate helper func

--- src/utils/getJobDate.js
+++ src/utils/getJobDate.js
@@ -1,39 +1,39 @@
 import moment from 'moment';
 
 export const getJobDate = (job_start_date, job_end_date) => {
-    let currentDate = new Date().getTime();
-    let startDate = new Date(
-        job_start_date.replace(/-/g, "/") + ":00"
+    const currentDate = new Date().getTime();
+    const jobStartDate = new Date(
+        job_start_date.replaceAll('-', "/")
     ).getTime();
-    let endDate = new Date(job_end_date.replace(/-/g, "/") + ":00").getTime();
-    if (currentDate < startDate) {
-        let diff = startDate - currentDate;
-        let days = Math.ceil(diff / (1000 * 3600 * 24));
+    const jobEndDate = new Date(job_end_date.replaceAll('-', "/")).getTime();
+    if (currentDate < jobStartDate) {
+        const diffInMs = jobStartDate - currentDate;
+        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
         return `${days} gün sonra yayınlanacak`;
-    } else if (currentDate >= startDate && currentDate <= endDate) {
-        let diff = currentDate - startDate;
-        let days = Math.ceil(diff / (1000 * 3600 * 24));
+    } else if (currentDate >= jobStartDate && currentDate <= jobEndDate) {
+        const diffInMs = currentDate - jobStartDate;
+        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
         return `${days} gündür yayında`;
     } else {
-        let diff = currentDate - endDate;
-        let days = Math.ceil(diff / (1000 * 3600 * 24));
+        const diffInMs = currentDate - jobEndDate;
+        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
         return `${days} gün önce yayından kaldırılmış`;
     }
 };
 
 export const getJobDateWithMomentJS = (job_start_date, job_end_date) => {
-    let currentDate = moment();
-    let startDate = moment(job_start_date, "YYYY-MM-DD HH:mm");
-    let endDate = moment(job_end_date, "YYYY-MM-DD HH:mm");
+    const currentDate = moment();
+    const jobStartDate = moment(job_start_date, "YYYY-MM-DD HH:mm");
+    const jobEndDate = moment(job_end_date, "YYYY-MM-DD HH:mm");
 
-    if (currentDate.isBefore(startDate)) {
-        let diff = startDate.diff(currentDate, 'days');
-        return `${diff} gün sonra yayınlanacak`;
-    } else if (currentDate.isBetween(startDate, endDate)) {
-        let diff = currentDate.diff(startDate, 'days');
-        return `${diff} gündür yayında`;
+    if (currentDate.isBefore(jobStartDate)) {
+        const days = jobStartDate.diff(currentDate, 'days');
+        return `${days} gün sonra yayınlanacak`;
+    } else if (currentDate.isBetween(jobStartDate, jobEndDate)) {
+        const days = currentDate.diff(jobStartDate, 'days');
+        return `${days} gündür yayında`;
     } else {
-        let diff = currentDate.diff(endDate, 'days');
-        return `${diff} gün önce yayından kaldırılmış`;
+        const days = currentDate.diff(jobEndDate, 'days');
+        return `${days} gün önce yayından kaldırılmış`;
     }
 }
