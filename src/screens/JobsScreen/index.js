9b050bdc73233dbef66a992bf7aeb7bd44c17342
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 11:22:45 +0300

    feat: add a search functionality on jobs

--- src/screens/JobsScreen/index.js
+++ src/screens/JobsScreen/index.js
@@ -1,5 +1,5 @@
 // components, styles
-import { FlatList, RefreshControl, Text, View } from "react-native";
+import { FlatList, RefreshControl, Text, TextInput, View } from "react-native";
 import Loading from "../../components/common/Loading";
 import JobItem from "../../components/common/JobItem";
 import AddJobFAB from "../../components/common/AddJobFAB";
@@ -12,66 +12,99 @@ import { useEffect, useState } from "react";
 import { getAllJobs } from "../../services/jobs";
 
 const JobsScreen = () => {
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [refreshLoading, setRefreshLoading] = useState(false);
+	const [jobs, setJobs] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [refreshLoading, setRefreshLoading] = useState(false);
+	const [search, setSearch] = useState("");
 
-  const fetchJobs = () => {
-    setLoading(true);
-    getAllJobs()
-      .then(setJobs)
-      .finally(() => setLoading(false));
-  };
+	const fetchJobs = () => {
+		setLoading(true);
+		getAllJobs()
+			.then(setJobs)
+			.finally(() => setLoading(false));
+	};
 
-  const refreshJobs = () => {
-    setRefreshLoading(true);
-    getAllJobs()
-      .then(setJobs)
-      .finally(() => setRefreshLoading(false));
-  };
+	const refreshJobs = () => {
+		setRefreshLoading(true);
+		getAllJobs()
+			.then(setJobs)
+			.finally(() => setRefreshLoading(false));
+	};
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
+	useEffect(() => {
+		fetchJobs();
+	}, []);
 
-  const listEmptyComponent = () => {
-    return (
-      <View style={styles.emptyListContainer}>
-        <Text style={styles.emptyListText}>İlanda olan hiç iş bulamadık!</Text>
-      </View>
-    );
-  };
+	const handleSearch = (value) => {
+		setSearch(value);
+	}
 
-  return (
-    <View style={styles.container}>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>İş Ara</Text>
-          </View>
-          <FlatList
-            data={jobs || []}
-            keyExtractor={(item) => `job/${item.id}`}
-            initialNumToRender={5}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => <JobItem job={item} />}
-            style={styles.flatListContainer}
-            ListEmptyComponent={listEmptyComponent}
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshLoading}
-                tintColor={"#DB4914"}
-                onRefresh={refreshJobs}
-              />
-            }
-          />
-          <AddJobFAB />
-        </>
-      )}
-    </View>
-  );
+	const filteredJobs = jobs.filter(job =>
+		job.job_title.toLowerCase().includes(search.toLowerCase()) ||
+		job.job_owner.toLowerCase().includes(search.toLowerCase())
+	);
+
+	const listEmptyComponent = () => {
+		return (
+			search.length > 0 && filteredJobs.length === 0 ?
+				<View style={styles.emptyListContainer}>
+					<Text style={styles.emptyListText}>
+						Arama kriterlerine uygun iş ilanı bulamadık!
+					</Text>
+				</View>
+				:
+				<View style={styles.emptyListContainer}>
+					<Text style={styles.emptyListText}>
+						İlanda olan hiç iş bulamadık!
+					</Text>
+				</View>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<View style={styles.headerContainer}>
+						<Text style={styles.headerText}>İş Ara</Text>
+						<TextInput
+							style={{
+								height: 40,
+								borderColor: '#4E586E',
+								borderWidth: 1,
+								padding: 10,
+								margin: 15,
+								borderRadius: 10,
+								color: "white"
+							}}
+							value={search}
+							onChangeText={handleSearch}
+							placeholder="Ünvan ve şirket ara..."
+						/>
+					</View>
+					<FlatList
+						data={filteredJobs || []}
+						keyExtractor={(item) => `job/${item.id}`}
+						initialNumToRender={5}
+						showsVerticalScrollIndicator={false}
+						renderItem={({ item }) => <JobItem job={item} />}
+						style={styles.flatListContainer}
+						ListEmptyComponent={listEmptyComponent}
+						refreshControl={
+							<RefreshControl
+								refreshing={refreshLoading}
+								tintColor={"#DB4914"}
+								onRefresh={refreshJobs}
+							/>
+						}
+					/>
+					<AddJobFAB />
+				</>
+			)}
+		</View>
+	);
 };
 
 export default JobsScreen;
--- src/screens/JobsScreen/styles.js
+++ src/screens/JobsScreen/styles.js
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         borderBottomWidth: 1,
         borderBottomColor: "#292D32",
+        marginTop: 20
     },
     headerText: {
         color: "white",
