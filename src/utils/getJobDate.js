5f55d1143e1165cacd0cf3161df9ddf3333e837f
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 15:01:32 +0300

    refactor: asign magic numbers to variable in getjobdate helper function

--- src/utils/getJobDate.js
+++ src/utils/getJobDate.js
@@ -1,6 +1,9 @@
 import moment from 'moment';
 
 export const getJobDate = (job_start_date, job_end_date) => {
+
+    const changeMStoDay = (1000 * 3600 * 24);
+
     const currentDate = new Date().getTime();
     const jobStartDate = new Date(
         job_start_date.replaceAll('-', "/")
@@ -8,15 +11,15 @@ export const getJobDate = (job_start_date, job_end_date) => {
     const jobEndDate = new Date(job_end_date.replaceAll('-', "/")).getTime();
     if (currentDate < jobStartDate) {
         const diffInMs = jobStartDate - currentDate;
-        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
+        const days = Math.ceil(diffInMs / changeMStoDay);
         return `${days} gün sonra yayınlanacak`;
     } else if (currentDate >= jobStartDate && currentDate <= jobEndDate) {
         const diffInMs = currentDate - jobStartDate;
-        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
+        const days = Math.ceil(diffInMs / changeMStoDay);
         return `${days} gündür yayında`;
     } else {
         const diffInMs = currentDate - jobEndDate;
-        const days = Math.ceil(diffInMs / (1000 * 3600 * 24));
+        const days = Math.ceil(diffInMs / changeMStoDay);
         return `${days} gün önce yayından kaldırılmış`;
     }
 };
