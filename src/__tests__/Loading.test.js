fc24c68eed8ce4d9172259f3560c0e53c79d738f
mguvensancaktar <mguvensancaktar@gmail.com>
2023-01-14 13:02:30 +0300

    test: add more test to loading component

--- src/__tests__/Loading.test.js
+++ src/__tests__/Loading.test.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { render } from 'react-native-testing-library';
 import Loading from '../components/common/Loading';
-import { NavigationContainer } from '@react-navigation/native';
-
+import { render } from '@testing-library/react-native';
+import { COLORS } from '../theme';
 
 describe('Loading', () => {
-    it('should render correctly', () => {
-        const { getByTestId } = render(
-            <NavigationContainer>
-                <Loading />
-            </NavigationContainer>
-        );
+    it('should render a loading indicator', () => {
+        const { getByTestId } = render(<Loading />);
         expect(getByTestId('loading')).toBeTruthy();
+    });
+
+    it('should include an activity indicator', () => {
+        const { getByTestId } = render(<Loading />);
+        expect(getByTestId('activity-indicator')).toBeTruthy();
+    });
+
+    it('should have the correct color', () => {
+        const { getByTestId } = render(<Loading />);
+        expect(getByTestId("activity-indicator").props.color).toBe(COLORS.secondary);
     });
 });
--- src/components/common/Loading/index.js
+++ src/components/common/Loading/index.js
@@ -6,7 +6,7 @@ import styles from "./styles";
 const Loading = () => {
   return (
     <View style={styles.loadingIndicator} testID="loading">
-      <ActivityIndicator size="large" color={COLORS.secondary} />
+      <ActivityIndicator size="large" color={COLORS.secondary} testID="activity-indicator" />
     </View>
   );
 };
